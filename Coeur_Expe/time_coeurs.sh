08989a3b49ba8d65a160d6edb821e65845e2fc5f
Nicolas Dugué <nicolas.dugue@loria.fr>
2015-11-02 20:39:41 +0100

    Removing density scripts.

--- Coeur_Expe/time_coeurs.sh
+++
@@ -1,6 +0,0 @@
-(time python simple-run.py graphes_renum/petster-hamster/out.petster-hamster_renum hamster_full 20 1) &>> TIME_COEURS_full
-(time python simple-run.py graphes_renum/ca-AstroPh/out.ca-AstroPh_renum astroph_full 20 1) &>> TIME_COEURS_full
-(time python simple-run.py graphes_renum/petster-hamster/out.petster-hamster_renum hamster_fast_20 20 5) &>> TIME_COEURS
-(time python simple-run.py graphes_renum/ca-AstroPh/out.ca-AstroPh_renum astroph_fast_20 20 5) &>> TIME_COEURS
-(time python simple-run.py graphes_renum/petster-hamster/out.petster-hamster_renum hamster_fast_40 40 5) &>> TIME_COEURS
-(time python simple-run.py graphes_renum/ca-AstroPh/out.ca-AstroPh_renum astroph_fast_40 40 5) &>> TIME_COEURS
